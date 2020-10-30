33e728b1721a0723fc6b3b245e6582e2112e5798
Isadora Decker-Lucke <isadoradl5@gmail.com>
2020-10-30 14:51:24 -0600

    parts 1-3 of project

--- db.js
+++ db.js
@@ -1,2 +1,11 @@
 /** Database setup for BizTime. */
 
+const {Client} = require('pg');
+
+const client = new Client({
+    connectionString: 'posgresql://biztime'
+});
+
+client.connect();
+
+module.exports = client;
